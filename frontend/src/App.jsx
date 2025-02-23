85ec8fcb99114c27f0f926409ac4bf52e0c9b842
karthik99 <jamikarthikeya99@gmail.com>
2025-02-23 16:28:31 +0530

    project completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
-import {useThemeStore} from "./store/useThemeStore"
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
